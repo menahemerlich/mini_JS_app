628d5163ca6fc3bd78c9c1ba0e4b0a6825cd140b
DavidPopovich3261 <8534397@gmail.com>
2025-12-07 10:02:03 +0200

    feat: hero says 'Welcome to HQ'

---
+++ index.js
@@ -0,0 +1,8 @@
+import { THEME_COLOR } from "./config.js";
+
+function getHeroTitle() {
+  return "Welcome to HQ";
+}
+
+console.log("HERO TITLE:", getHeroTitle());
+console.log("THEME COLOR:", THEME_COLOR);
